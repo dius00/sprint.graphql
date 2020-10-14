b7d9750d0fc303395ab5ea5d9b7bdb5c91261859
Jose M. Torres <jmtorresr@gmail.com>
2020-10-14 11:44:07 +0900

    Set up the schema for the GraphQL
    Co-authored-by: dhequex <dhequex@users.noreply.github.com>
    Co-authored-by: dius00 <dius00@users.noreply.github.com>

--- server/schema.js
+++ server/schema.js
@@ -4,10 +4,49 @@ const typeDefs = gql`
   # Your schema goes here.
   # The schema should model the full data object available!
 
-  type Pokemon {
-    id: String
+  type Attack {
+    name: String!
+    type: String!
+    damage: Int!
+  }
+
+  type AttackClass {
+    fast: [Attack]
+    special: [Attack]
+  }
+
+  type WeightandHeight {
+    minimum: String!
+    maximum: String!
+  }
+
+  type IdplusName {
+    id: Int!
     name: String!
   }
+
+  type EvolReq {
+    amount: Int!
+    name: String!
+  }
+
+  type Pokemon {
+    id: String!
+    name: String!
+    classification: String!
+    types: [String!]
+    resistant: [String!]
+    weaknesses: [String!]
+    weight: WeightandHeight
+    height: WeightandHeight
+    fleeRate: Float!
+    evolutionRequirements: EvolReq
+    evolutions: [IdplusName]
+    maxCP: Int!
+    maxHP: Int!
+    attacks: AttackClass
+  }
+
   type Query {
     Pokemons: [Pokemon]
     Pokemon(name: String!): Pokemon
